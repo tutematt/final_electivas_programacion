7566e66539eff41d00d5b0d791f1a8aab82db62f
matiasdavidrodoni <matiasrodoni@outlook.com.ar>
2023-02-13 21:00:51 -0300

    Un mensaje de prueba para comprobar si funciona bien el push

--- app/src/main/java/com/example/final_electivas_programacion/MainActivity.java
+++ app/src/main/java/com/example/final_electivas_programacion/MainActivity.java
@@ -19,7 +19,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    //Prueba
+    //Prueba de cambios
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
