0eeb2b943823b68bcda666a64ffef4be94b09744
matiasdavidrodoni <matiasrodoni@outlook.com.ar>
2023-02-14 20:33:17 -0300

    -Pantalla de Registro para personas.

--- app/src/main/java/com/example/final_electivas_programacion/PantallaRegistrarse.java
+++ app/src/main/java/com/example/final_electivas_programacion/PantallaRegistrarse.java
@@ -1,4 +1,14 @@
 package com.example.final_electivas_programacion;
 
-public class PantallaRegistrarse {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PantallaRegistrarse extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_registro);
+    }
 }
